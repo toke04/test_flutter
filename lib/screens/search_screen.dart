199f65ff6f8390641bd04bc0845823d2d28cde72
toke <54713809+syo-tokeshi@users.noreply.github.com>
2024-05-25 13:17:25 +0900

    httpリクエストを叩いて記事一覧を作れるようにした

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -22,3 +22,29 @@ class _SearchScreenState extends State<SearchScreen> {
     );
   }
 }
+
+Future<List<Article>> searchQiita(String keyword) async {
+  // 1. http通信に必要なデータを準備をする
+  //   - URL、クエリパラメータの設定
+  final uri = Uri.https('qiita.com', '/api/v2/items', {
+    'query': 'title:$keyword',
+    'per_page': '10',
+  });
+  //   - アクセストークンの取得
+  final String token = dotenv.env['QIITA_ACCESS_TOKEN'] ?? '';
+
+  // 2. Qiita APIにリクエストを送る
+  final http.Response res = await http.get(uri, headers: {
+    'Authorization': 'Bearer $token',
+  });
+
+  // 3. 戻り値をArticleクラスの配列に変換
+  // 4. 変換したArticleクラスの配列を返す(returnする)
+  if (res.statusCode == 200) {
+    // レスポンスをモデルクラスへ変換
+    final List<dynamic> body = jsonDecode(res.body);
+    return body.map((dynamic json) => Article.fromJson(json)).toList();
+  } else {
+    return [];
+  }
+}
