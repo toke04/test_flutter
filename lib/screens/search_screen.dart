52684907c282b08887bd19cc6f3735f7ccdfbbee
toke <54713809+syo-tokeshi@users.noreply.github.com>
2024-05-25 13:12:50 +0900

    qiita apiから取得したデータを表示する画面の作成

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http; // httpという変数を通して、httpパッケージにアクセス
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import '/models/article.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Qiita Search'),
+      ),
+      body: Container(),
+    );
+  }
+}
